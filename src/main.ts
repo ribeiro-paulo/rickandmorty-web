591b7ca02a429b5df8e8e3f86f7af138ce5be576
Paulo Ribeiro <ribeiro.paulotm@gmail.com>
2021-12-26 19:18:47 -0300

    feat: config project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false;
 
@@ -8,3 +16,4 @@ new Vue({
   router,
   render: (h) => h(App),
 }).$mount('#app');
+
